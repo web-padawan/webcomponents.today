f2aea3c32f451769fc01a30b625589390d709ac1
web-padawan <iamkulykov@gmail.com>
2021-05-16 19:36:59 +0300

    Add thumbnails, update cards styles

--- content/shadow-dom/shadow-dom.11tydata.js
+++ content/shadow-dom/shadow-dom.11tydata.js
@@ -6,24 +6,28 @@ module.exports = {
     {
       url: "https://bitsofco.de/what-is-the-shadow-dom/",
       title: "What is the Shadow DOM?",
+      thumbnail: "bitsofcode.png",
       quote:
         "Although only recently specified for use by web authors, the shadow DOM has been used by user agents for years.",
     },
     {
       url: "https://developers.google.com/web/fundamentals/web-components/shadowdom",
       title: "Shadow DOM v1: Self-Contained Web Components",
+      thumbnail: "fundamentals.png",
       quote:
         "Shadow DOM allows to bundle CSS with markup, hide implementation details, and author self-contained components.",
     },
     {
       url: "https://developer.mozilla.org/en-US/docs/Web/Web_Components/Using_shadow_DOM",
       title: "Using shadow DOM",
+      thumbnail: "mdn.png",
       quote:
         "The Shadow DOM API is a key part of web components, providing a way to attach a hidden separated DOM to an element.",
     },
     {
       url: "https://pm.dartus.fr/blog/a-complete-guide-on-shadow-dom-and-event-propagation/",
       title: "A complete guide on shadow DOM and event propagation",
+      thumbnail: "pmdartus.png",
       quote:
         "Shadow DOM directly influences how events propagate through the DOM. This article gives an overview of how it works.",
     },
