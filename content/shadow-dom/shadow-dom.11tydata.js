d5a3e643e9a4fd6e718e0c0488908529292a78e1
web-padawan <iamkulykov@gmail.com>
2021-05-16 16:20:05 +0300

    Update quotes, add new article

--- content/shadow-dom/shadow-dom.11tydata.js
+++ content/shadow-dom/shadow-dom.11tydata.js
@@ -6,31 +6,26 @@ module.exports = {
     {
       url: "https://bitsofco.de/what-is-the-shadow-dom/",
       title: "What is the Shadow DOM?",
-      quote: `In the past few years, you may have heard of terms like "Shadow DOM" and
-        "Virtual DOM". These, although of course related to the original DOM, refer to very
-        different concepts. In this article, I will cover what, exactly, the shadow DOM is and
-        how it differs from the original DOM.`,
+      quote:
+        "Although only recently specified for use by web authors, the shadow DOM has been used by user agents for years.",
     },
     {
       url: "https://developers.google.com/web/fundamentals/web-components/shadowdom",
       title: "Shadow DOM v1: Self-Contained Web Components",
-      quote: `Shadow DOM fixes CSS and DOM. It introduces scoped styles to the web platform.
-        Without tools or naming conventions, you can bundle CSS with markup, hide implementation
-        details, and author self-contained components in vanilla JavaScript.`,
-    },
-    {
-      url: "https://webkit.org/blog/4096/introducing-shadow-dom-api/",
-      title: "Introducing Slot-Based Shadow DOM API",
-      quote: `Shadow DOM is a part of Web Components, a set of specifications that were
-        initially proposed by Google to enable the creation of reusable widgets and components
-        on the Web.`,
+      quote:
+        "Shadow DOM allows to bundle CSS with markup, hide implementation details, and author self-contained components.",
     },
     {
       url: "https://developer.mozilla.org/en-US/docs/Web/Web_Components/Using_shadow_DOM",
       title: "Using shadow DOM",
-      quote: `An important aspect of web components is encapsulation — being able to keep the
-        markup, styles, and behavior hidden and separate from other code. The Shadow DOM API is
-        a key part of this, providing a way to attach a hidden separated DOM to an element.`,
+      quote:
+        "The Shadow DOM API is a key part of web components, providing a way to attach a hidden separated DOM to an element.",
+    },
+    {
+      url: "https://pm.dartus.fr/blog/a-complete-guide-on-shadow-dom-and-event-propagation/",
+      title: "A complete guide on shadow DOM and event propagation",
+      quote:
+        "Shadow DOM directly influences how events propagate through the DOM. This article gives an overview of how it works.",
     },
   ],
 };
