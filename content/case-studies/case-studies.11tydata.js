425bf04a0af246e973a24196c5437ad0652a123d
web-padawan <iamkulykov@gmail.com>
2021-06-21 19:31:11 +0300

    Add Case Studies page content

---
+++ content/case-studies/case-studies.11tydata.js
@@ -0,0 +1,71 @@
+module.exports = {
+  url: "case-studies",
+  header: "Case Studies",
+  description: "Developers from different engineering teams share their experiences of using Web Components.",
+  items: [
+    {
+      url: "https://dev.to/thatjoemoore/bringing-order-to-web-design-chaos--3fhb",
+      title: "Bringing Order to Web Design Chaos with Web Components",
+      thumbnail: "devto.png",
+      quote: "This has been a really fun project to work on, forcing us to tackle a lot of interesting challenges.",
+    },
+    {
+      url: "https://medium.com/8451/how-we-chose-to-build-our-design-system-using-stenciljs-web-components-4878c36743c5",
+      title: "How we chose to build our Design System using StencilJS Web Components",
+      thumbnail: "medium.svg",
+      quote:
+        "We looked at several options to build out web components including Angular Elements, LitElement and StencilJS.",
+    },
+    {
+      url: "https://github.blog/2021-05-04-how-we-use-web-components-at-github/",
+      title: "How we use Web Components at GitHub",
+      thumbnail: "github.svg",
+      quote: "Web Components offered better portability and encapsulation than our existing JavaScript behaviors.",
+    },
+    {
+      url: "https://medium.com/@Danetag/implementing-a-design-language-system-with-stencil-js-515432918eb5",
+      title: "Implementing a Design Language System with Stencil.js",
+      thumbnail: "medium.svg",
+      quote:
+        "Stencil leverages Web Components with polyfills and fallbacks. The best part? It’s really, really simple to setup.",
+    },
+    {
+      url: "https://medium.com/ing-blog/ing-open-sources-lion-a-library-for-performant-accessible-flexible-web-components-22ad165b1d3d",
+      title: "ING open sources Lion: A library of performant, accessible & flexible Web Components",
+      thumbnail: "medium.svg",
+      quote: "ING has a long and rich history of building Web Components.",
+    },
+    {
+      url: "https://bitworking.org/news/2019/07/looking-back-on-five-years-of-web-components/",
+      title: "Looking back on five years of web components",
+      thumbnail: "bitworking.svg",
+      quote: "Instead of picking a monolithic solution like a web framework, you just pick the pieces you need.",
+    },
+    {
+      url: "https://medium.com/@banked/shipping-web-components-in-2020-ff387c44e1a4",
+      title: "Shipping Web Components in 2020",
+      thumbnail: "medium.svg",
+      quote: "Web Components seemed to be perfectly designed for our use case and the challenges we were running into.",
+    },
+    {
+      url: "https://briangrinstead.com/blog/firefox-webcomponents/",
+      title: "The Firefox UI is now built with Web Components",
+      thumbnail: "bgrins.png",
+      quote:
+        "The Firefox UI can be thought of as a very large single page app. It was built with DOM and JS from the start.",
+    },
+    {
+      url: "https://about.gitlab.com/blog/2021/05/03/using-web-components-to-encapsulate-css-and-resolve-design-system-conflicts/",
+      title: "Using web components to encapsulate CSS and resolve design system conflicts",
+      thumbnail: "gitlab.svg",
+      quote: "We used the shadow DOM to encapsulate CSS. Templates and slots allowed us to use existing HTML.",
+    },
+    {
+      url: "https://web.dev/how-we-build-webdev-and-use-web-components/",
+      title: "web.dev engineering blog #1: How we build the site and use Web Components",
+      thumbnail: "webdev.png",
+      quote:
+        "web.dev's custom elements mostly inherit from LitElement, which provides a simple base for complex components.",
+    },
+  ],
+};
