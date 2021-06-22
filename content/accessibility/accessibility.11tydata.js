5f61ce2201153fde71f42482bc5f5623145109f4
web-padawan <iamkulykov@gmail.com>
2021-06-22 22:13:22 +0300

    Add Accessibility page content

---
+++ content/accessibility/accessibility.11tydata.js
@@ -0,0 +1,45 @@
+module.exports = {
+  url: "accessibility",
+  header: "Accessibility",
+  description: "It is really important to make web components fully accessible. Sometimes is might be tricky.",
+  items: [
+    {
+      url: "https://developer.salesforce.com/blogs/2020/01/accessibility-for-web-components.html",
+      title: "Accessibility for Web Components",
+      thumbnail: "salesforce.svg",
+      quote:
+        "When architecting web components, be sure to include these considerations so everyone can use your components.",
+    },
+    {
+      url: "https://robdodson.me/posts/the-future-of-accessibility-for-custom-elements/",
+      title: "The future of accessibility for custom elements",
+      thumbnail: "robdodson.png",
+      quote:
+        "When you define a new tag, the browser really has no way of knowing if you’re trying to build a button, or a slider.",
+    },
+    {
+      url: "https://www.erikkroes.nl/blog/accessibility/the-guide-to-accessible-web-components-draft/",
+      title: "The Guide to Accessible Web Components",
+      thumbnail: "erikkroes.png",
+      quote: "Building proper UI Web Components can be quite a task, especially if you want them to be accessible.",
+    },
+    {
+      url: "https://github.com/devongovett/wc-hooks",
+      title: "wc-hooks",
+      thumbnail: "github.svg",
+      quote: "An experiment to try to use React Aria hooks in vanilla web components by shimming the React Hooks API.",
+    },
+    {
+      url: "https://www.24a11y.com/2019/web-components-and-the-aom/",
+      title: "Web Components and the Accessibility Object model",
+      thumbnail: "24a11y.png",
+      quote: "The AOM will make accessibility a first class citizen of the extensible web – exactly what it should be.",
+    },
+    {
+      url: "https://www.24a11y.com/2018/web-components-still-need-to-be-accessible/",
+      title: "Web components still need to be accessible",
+      thumbnail: "24a11y.png",
+      quote: "Web components render as HTML, and either that HTML works or it doesn’t.",
+    },
+  ],
+};
--- content/index.11tydata.js
+++ content/index.11tydata.js
@@ -1,9 +1,10 @@
+const a11y = require("./accessibility/accessibility.11tydata.js");
 const caseStudies = require("./case-studies/case-studies.11tydata.js");
 const elements = require("./custom-elements/custom-elements.11tydata.js");
 const shadowDOM = require("./shadow-dom/shadow-dom.11tydata.js");
 
 module.exports = {
-  items: [...caseStudies.items, ...elements.items, ...shadowDOM.items].sort((a, b) => {
+  items: [...a11y.items, ...caseStudies.items, ...elements.items, ...shadowDOM.items].sort((a, b) => {
     const textA = a.title.toUpperCase();
     const textB = b.title.toUpperCase();
     return textA < textB ? -1 : textA > textB ? 1 : 0;
