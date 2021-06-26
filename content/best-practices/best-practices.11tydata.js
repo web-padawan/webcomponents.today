397fd497cfd24f3e4c2e7529e0886113b24c52e5
web-padawan <iamkulykov@gmail.com>
2021-06-26 17:27:38 +0300

    Add Best Practices page content

---
+++ content/best-practices/best-practices.11tydata.js
@@ -0,0 +1,60 @@
+module.exports = {
+  url: "best-practices",
+  header: "Best Practices",
+  description: "Useful tips and recommendations helping to build and publish high quality Web Components.",
+  items: [
+    {
+      url: "https://medium.com/ing-blog/best-practices-to-build-web-components-6d517923fba4",
+      title: "Best practices to build Web Components",
+      thumbnail: "medium.svg",
+      quote:
+        "An overview of the key things that you should take into account when you start building a new web component.",
+    },
+    {
+      url: "https://developers.google.com/web/fundamentals/web-components/best-practices",
+      title: "Custom Element Best Practices",
+      thumbnail: "fundamentals.png",
+      quote: "A checklist that breaks down all the things we think it takes to be a well behaved custom element.",
+    },
+    {
+      url: "https://open-wc.org/guides/developing-components/publishing/",
+      title: "Developing Components: Publishing",
+      thumbnail: "open-wc.png",
+      quote:
+        "A set of recommendations to ensure the code you publish to npm is as easy for your users to consume as possible.",
+    },
+    {
+      url: "https://w3ctag.github.io/webcomponents-design-guidelines/",
+      title: "Guidelines for creating web platform compatible components",
+      thumbnail: "w3ctag.png",
+      quote:
+        "A set of common guidelines to make sure your custom web components fit in well with the rest of the platform.",
+    },
+    {
+      url: "https://justinfagnani.com/2019/11/01/how-to-publish-web-components-to-npm/",
+      title: "How to Publish Web Components to npm",
+      thumbnail: "justinfagnani.svg",
+      quote: "A checklist that attempts to maximize compatibility, standards compliance, flexibility, and usefulness.",
+    },
+    {
+      url: "https://css-tricks.com/making-web-components-for-different-contexts/",
+      title: "Making Web Components for Different Contexts",
+      thumbnail: "css-tricks.svg",
+      quote: "How to work with web components, what to consider when making them, and how to embrace them.",
+    },
+    {
+      url: "https://github.com/basic-web-components/components-dev/wiki/Ten-Principles-for-Great-General-Purpose-Web-Components",
+      title: "Ten Principles for Great General Purpose Web Components",
+      thumbnail: "github.svg",
+      quote:
+        "A set of principles for creating general-purpose web components that can be adopted in a wide range of sites.",
+    },
+    {
+      url: "https://github.com/webcomponents/gold-standard/wiki",
+      title: "The Gold Standard Checklist for Web Components",
+      thumbnail: "github.svg",
+      quote:
+        "A checklist for web components that aspire to be as flexible, reliable, and useful as the standard HTML elements.",
+    },
+  ],
+};
--- content/index.11tydata.js
+++ content/index.11tydata.js
@@ -1,12 +1,15 @@
 const a11y = require("./accessibility/accessibility.11tydata.js");
+const bestPractices = require("./best-practices/best-practices.11tydata.js");
 const caseStudies = require("./case-studies/case-studies.11tydata.js");
 const elements = require("./custom-elements/custom-elements.11tydata.js");
 const shadowDOM = require("./shadow-dom/shadow-dom.11tydata.js");
 
 module.exports = {
-  items: [...a11y.items, ...caseStudies.items, ...elements.items, ...shadowDOM.items].sort((a, b) => {
-    const textA = a.title.toUpperCase();
-    const textB = b.title.toUpperCase();
-    return textA < textB ? -1 : textA > textB ? 1 : 0;
-  }),
+  items: [...a11y.items, ...bestPractices.items, ...caseStudies.items, ...elements.items, ...shadowDOM.items].sort(
+    (a, b) => {
+      const textA = a.title.toUpperCase();
+      const textB = b.title.toUpperCase();
+      return textA < textB ? -1 : textA > textB ? 1 : 0;
+    }
+  ),
 };
