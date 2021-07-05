598312b60a12defa295991e7e9de6b29240ef505
web-padawan <iamkulykov@gmail.com>
2021-07-05 18:44:38 +0300

    Add Codelabs page content

---
+++ content/codelabs/codelabs.11tydata.js
@@ -0,0 +1,56 @@
+module.exports = {
+  url: "codelabs",
+  header: "Codelabs",
+  description: "Step by step tutorials that help to learn how to build Web Components through practical examples.",
+  items: [
+    {
+      url: "https://codelabs.developers.google.com/codelabs/the-lit-path#0",
+      title: "From Web Component to Lit Element",
+      thumbnail: "fundamentals.png",
+      quote:
+        "In this codelab, you will learn about the concepts of Web Components and how to build them with LitElement.",
+    },
+    {
+      url: "https://codelabs.developers.google.com/codelabs/lit-brick-viewer#0",
+      title: "Build a Brick Viewer with lit-element",
+      thumbnail: "fundamentals.png",
+      quote: "In this codelab, you will learn how to build a web component to display brick models.",
+    },
+    {
+      url: "https://codelabs.developers.google.com/codelabs/lit-story-viewer#0",
+      title: "Build a Story Component with lit-element",
+      thumbnail: "fundamentals.png",
+      quote: "In this codelab, you will learn how to build a build a story component with lit-element and TypeScript.",
+    },
+    {
+      url: "https://codelabs.developers.google.com/codelabs/lit-2-for-react-devs#0",
+      title: "Lit for React Developers",
+      thumbnail: "fundamentals.png",
+      quote: "In this codelab, you will learn how to convert React component concepts to their Lit analogs.",
+    },
+    {
+      url: "https://open-wc.org/codelabs/basics/web-components.html#0",
+      title: "Web Components: basics",
+      thumbnail: "open-wc.png",
+      quote: "In this codelab, you will learn about the basics of Web Components and how they work.",
+    },
+    {
+      url: "https://open-wc.org/codelabs/basics/lit-html.html#0",
+      title: "lit-html & lit-element: basics",
+      thumbnail: "open-wc.png",
+      quote: "In this codelab, you will learn the basics of building web components using lit-html and lit-element.",
+    },
+    {
+      url: "https://open-wc.org/codelabs/intermediate/lit-html.html#0",
+      title: "lit-html & lit-element: intermediate",
+      thumbnail: "open-wc.png",
+      quote: "This codelab is a followup from the lit-html & lit-element basics codelab.",
+    },
+    {
+      url: "https://kiosk-dot-codelabs-site.appspot.com/codelabs/image-styling-web-components/index.html#0",
+      title: "Image Styling with Web Components",
+      thumbnail: "codelabs.png",
+      quote: "In this codelab, you will learn how to create a vanilla web component for styling images.",
+    },
+  ],
+};
