ca978f524fe9f1b3bf9641ff67a18966cccc20fb
web-padawan <iamkulykov@gmail.com>
2022-11-06 16:28:00 +0200

    Add more a11y content, sort articles

--- content/accessibility/accessibility.11tydata.js
+++ content/accessibility/accessibility.11tydata.js
@@ -11,6 +11,25 @@ module.exports = {
         "When architecting web components, be sure to include these considerations so everyone can use your components.",
     },
     {
+      url: "https://coryrylan.com/blog/accessibility-with-id-referencing-and-shadow-dom",
+      title: "Accessibility with ID Referencing and Shadow DOM",
+      thumbnail: "cory-rylan.png",
+      quote: "About the most common mistake when making accessible components, id, and label associations.",
+    },
+    {
+      url: "https://nolanlawson.com/2022/06/14/dialogs-and-shadow-dom-can-we-make-it-accessible/",
+      title: "Dialogs and shadow DOM: can we make it accessible?",
+      thumbnail: "nolan-lawson.png",
+      quote: "A look at getting native dialogs to play nicely with shadow DOM.",
+    },
+    {
+      url: "https://nolanlawson.com/2021/02/13/managing-focus-in-the-shadow-dom/",
+      title: "Managing focus in the shadow DOM",
+      thumbnail: "nolan-lawson.png",
+      quote:
+        "Managing focus in the DOM has never been particularly easy, and shadow DOM just makes things that much trickier.",
+    },
+    {
       url: "https://robdodson.me/posts/the-future-of-accessibility-for-custom-elements/",
       title: "The future of accessibility for custom elements",
       thumbnail: "robdodson.png",
@@ -36,23 +55,16 @@ module.exports = {
       quote: "The AOM will make accessibility a first class citizen of the extensible web – exactly what it should be.",
     },
     {
+      url: "https://vaadin.com/blog/web-components-design-systems-and-accessibility-recording-available",
+      title: "Web components, design systems and accessibility",
+      thumbnail: "vaadin.png",
+      quote: "Live stream recording with engineers and product owners from Adobe, IBM, SAP, Vaadin, and ING.",
+    },
+    {
       url: "https://www.24a11y.com/2018/web-components-still-need-to-be-accessible/",
       title: "Web components still need to be accessible",
       thumbnail: "24a11y.png",
       quote: "Web components render as HTML, and either that HTML works or it doesn’t.",
-    },
-    {
-      url: "https://nolanlawson.com/2021/02/13/managing-focus-in-the-shadow-dom/",
-      title: "Managing focus in the shadow DOM",
-      thumbnail: "nolan-lawson.png",
-      quote:
-        "Managing focus in the DOM has never been particularly easy, and shadow DOM just makes things that much trickier.",
-    },
-    {
-      url: "https://nolanlawson.com/2022/06/14/dialogs-and-shadow-dom-can-we-make-it-accessible/",
-      title: "Dialogs and shadow DOM: can we make it accessible?",
-      thumbnail: "nolan-lawson.png",
-      quote: "A look at getting native dialogs to play nicely with shadow DOM.",
     },
   ],
 };
