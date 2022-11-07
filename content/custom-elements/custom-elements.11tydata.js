8afa94b1e5d50e999c8bd03c0ef1558e212550a0
web-padawan <iamkulykov@gmail.com>
2022-11-07 12:23:00 +0200

    Update and sort custom elements page

--- content/custom-elements/custom-elements.11tydata.js
+++ content/custom-elements/custom-elements.11tydata.js
@@ -4,23 +4,23 @@ module.exports = {
   description: "Custom Elements provide a way for authors to build their own fully-featured DOM elements.",
   items: [
     {
+      url: "https://css-tricks.com/a-guide-to-custom-elements-for-react-developers/",
+      title: "A Guide to Custom Elements for React Developers",
+      thumbnail: "css-tricks.svg",
+      quote:
+        "Custom elements can offer the general benefits of React components without being tied to a specific framework.",
+    },
+    {
+      url: "https://github.com/shawnbot/custom-elements",
+      title: "All about HTML Custom Elements",
+      thumbnail: "github.svg",
+      quote: "Everything you need to know about Custom Elements.",
+    },
+    {
       url: "https://web.dev/custom-elements-v1/",
       title: "Custom Elements v1: Reusable Web Components",
       thumbnail: "webdev.png",
       quote: "With Custom Elements, web developers can create new HTML tags.",
-    },
-    {
-      url: "https://developer.mozilla.org/en-US/docs/Web/Web_Components/Using_custom_elements",
-      title: "Using custom elements",
-      thumbnail: "mdn.png",
-      quote: "This article introduces the use of the Custom Elements API.",
-    },
-    {
-      url: "https://html.spec.whatwg.org/multipage/custom-elements.html",
-      title: "HTML Standard: Custom elements",
-      thumbnail: "whatwg.svg",
-      quote:
-        'Custom elements are part of a larger effort to "rationalise the platform", by explaining existing platform features.',
     },
     {
       url: "https://javascript.info/custom-elements",
@@ -30,10 +30,17 @@ module.exports = {
         "We can create custom HTML elements, described by our class, with its own methods, properties, and events.",
     },
     {
-      url: "https://github.com/shawnbot/custom-elements",
-      title: "All about HTML Custom Elements",
-      thumbnail: "github.svg",
-      quote: "Everything you need to know about Custom Elements.",
+      url: "https://custom-elements-everywhere.com",
+      title: "Custom Elements Everywhere",
+      thumbnail: "cee.svg",
+      quote:
+        "A suite of tests for different frameworks to identify interoperability issues, and highlight potential fixes.",
+    },
+    {
+      url: "https://nolanlawson.com/2021/08/03/handling-properties-in-custom-element-upgrades/",
+      title: "Handling properties in custom element upgrades",
+      thumbnail: "nolan-lawson.png",
+      quote: "About a tricky situation with properties and the component lifecycle.",
     },
     {
       url: "https://gist.github.com/WebReflection/ec9f6687842aa385477c4afca625bbf4",
@@ -42,17 +49,23 @@ module.exports = {
       quote: "A collection of common patterns for using with Custom Elements.",
     },
     {
-      url: "https://css-tricks.com/a-guide-to-custom-elements-for-react-developers/",
-      title: "A Guide to Custom Elements for React Developers",
-      thumbnail: "css-tricks.svg",
+      url: "https://html.spec.whatwg.org/multipage/custom-elements.html",
+      title: "HTML Standard: Custom elements",
+      thumbnail: "whatwg.svg",
       quote:
-        "Custom elements can offer the general benefits of React components without being tied to a specific framework.",
+        'Custom elements are part of a larger effort to "rationalise the platform", by explaining existing platform features.',
     },
     {
       url: "https://component.kitchen/blog/posts/supporting-both-automatic-and-manual-registration-of-custom-elements",
       title: "Supporting both automatic and manual registration of custom elements",
       thumbnail: "component-kitchen.svg",
       quote: "While auto-registering components are convenient, they lead to some problems.",
+    },
+    {
+      url: "https://developer.mozilla.org/en-US/docs/Web/Web_Components/Using_custom_elements",
+      title: "Using custom elements",
+      thumbnail: "mdn.png",
+      quote: "This article introduces the use of the Custom Elements API.",
     },
   ],
 };
