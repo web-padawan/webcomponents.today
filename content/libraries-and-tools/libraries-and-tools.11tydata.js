bf6c0d04666070665fdbdd866b1ad5462345efbf
Jochen Kühner <jochen.kuehner@gmx.de>
2022-12-28 16:30:29 +0100

    Add node-projects repos to Libraries and Tools (#5)

--- content/libraries-and-tools/libraries-and-tools.11tydata.js
+++ content/libraries-and-tools/libraries-and-tools.11tydata.js
@@ -16,6 +16,12 @@ module.exports = {
       quote: "A library for creating web components with React-inspired syntax.",
     },
     {
+      url: "https://github.com/node-projects/base-custom-webcomponent",
+      title: "base-custom-webcomponent",
+      thumbnail: "node-projects.png",
+      quote: "A simple base class for creating Web Components in TypeScript with custom template and bindings syntax.",
+    },
+    {
       url: "https://catalyst.rocks",
       title: "Catalyst",
       thumbnail: "catalyst.png",
@@ -112,6 +118,12 @@ module.exports = {
       title: "Web Components Compiler",
       thumbnail: "wcc.png",
       quote: "Compiler to make server-side rendering of native web components easier.",
+    },
+    {
+      url: "https://github.com/node-projects/web-component-designer",
+      title: "web-component-designer",
+      thumbnail: "node-projects.png",
+      quote: "WYSIWYG tool for creating web components or HTML pages.",
     },
     {
       url: "https://github.com/11ty/webc",
