f48c1234ccb3018fb19e54c9dc89e8e78718a3fc
web-padawan <iamkulykov@gmail.com>
2023-01-11 21:22:46 +0200

    Add Beginner's Guide page under Fundamentals

---
+++ content/beginners-guide/beginners-guide.11tydata.js
@@ -0,0 +1,75 @@
+module.exports = {
+  url: "beginners-guide",
+  header: "Beginner's Guide",
+  description: "If you are new to Web Components, here are some resources to help you get started.",
+  items: [
+    {
+      url: "https://webcomponents.guide/learn/components/autonomous-vs-customized-elements/",
+      title: "Autonomous vs Customized Elements",
+      thumbnail: "wc-guide.svg",
+      quote:
+        "Choosing a type of component to build depends on a lot of factors. Here are some considerations to think about.",
+    },
+    {
+      url: "https://css-tricks.com/creating-a-custom-element-from-scratch/",
+      title: "Creating a Custom Element from Scratch",
+      thumbnail: "css-tricks.svg",
+      quote:
+        "This article describes how to create a custom modal dialog from the ground up using a <template> element.",
+    },
+    {
+      url: "https://webcomponents.guide/learn/components/",
+      title: "Defining a Component",
+      thumbnail: "wc-guide.svg",
+      quote:
+        "Creating a JavaScript class and registering it in the Custom Elements Registry to make the browser use it.",
+    },
+    {
+      url: "https://webcomponents.guide/learn/components/initializing-your-component/",
+      title: "Initializing Your Component",
+      thumbnail: "wc-guide.svg",
+      quote:
+        "Components can have styles, template, state etc. All these things need to be initialized using different techniques.",
+    },
+    {
+      url: "https://webcomponents.guide/learn/components/lifecycle-reference/",
+      title: "Lifecycle Reference",
+      thumbnail: "wc-guide.svg",
+      quote:
+        "Custom Elements have several method names which are called periodically through the life cycle of the element.",
+    },
+    {
+      url: "https://webcomponents.guide/learn/components/naming-your-components/",
+      title: "Naming your components",
+      thumbnail: "wc-guide.svg",
+      quote:
+        "Requirements for a valid tag name, and some tips and tricks on how to pick a good name for a custom element.",
+    },
+    {
+      url: "https://webcomponents.guide/learn/components/rendering/",
+      title: "Rendering",
+      thumbnail: "wc-guide.svg",
+      quote:
+        "While a component could make changes to the DOM the element is within, there's a better way: using Shadow DOM.",
+    },
+    {
+      url: "https://dev.to/thepassle/web-components-from-zero-to-hero-4n4m",
+      title: "Web Components: from zero to hero",
+      thumbnail: "devto.png",
+      quote: "This blogpost is intended to get a better grasp of the basics of web components.",
+    },
+    {
+      url: "https://dev.to/alangdm/where-to-begin-building-web-components-the-basics-3b78",
+      title: "Where to begin building Web Components? - The Basics",
+      thumbnail: "devto.png",
+      quote:
+        "For anyone trying to begin building Web Components just even finding out where to start is pretty difficult.",
+    },
+    {
+      url: "https://dev.to/alangdm/where-to-begin-building-web-components-class-based-libraries-18m6",
+      title: "Where to begin building Web Components? - Class-based Libraries",
+      thumbnail: "devto.png",
+      quote: "The class-based pattern is commonly used by Web Components libraries.",
+    },
+  ],
+};
