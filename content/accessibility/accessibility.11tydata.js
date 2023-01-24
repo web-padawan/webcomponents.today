e743bedfcaef54b4530ca15cde7bbb4fb8c6b9a3
web-padawan <iamkulykov@gmail.com>
2023-01-24 22:45:55 +0200

    Add two new articles to the Accessibility page

--- content/accessibility/accessibility.11tydata.js
+++ content/accessibility/accessibility.11tydata.js
@@ -11,10 +11,23 @@ module.exports = {
         "When architecting web components, be sure to include these considerations so everyone can use your components.",
     },
     {
+      url: "https://sap.github.io/ui5-webcomponents/playground/advanced/accessibility/",
+      title: "Accessibility in UI5 Web Components",
+      thumbnail: "ui5.png",
+      quote:
+        "As the Web Components are a relatively new standard, there are still some gaps in regard to accessibility.",
+    },
+    {
       url: "https://coryrylan.com/blog/accessibility-with-id-referencing-and-shadow-dom",
       title: "Accessibility with ID Referencing and Shadow DOM",
       thumbnail: "cory-rylan.png",
       quote: "About the most common mistake when making accessible components, id, and label associations.",
+    },
+    {
+      url: "https://htmlwithsuperpowers.netlify.app/faq/accessibility.html",
+      title: "Are Web Components Accessible?",
+      thumbnail: "html-with-superpowers.png",
+      quote: "Short answer: yes, but there's a couple of known issues around forms, labelling, and focus behavior.",
     },
     {
       url: "https://nolanlawson.com/2022/06/14/dialogs-and-shadow-dom-can-we-make-it-accessible/",
